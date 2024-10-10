3b8a81ae2852c2364f098226c031e982de0d566a
Michael Nelson <zsc.michaeln@gmail.com>
2024-10-10 09:56:55 -0500

    Add comments more detailed comments clean up code Move determining suit color to class Suit (makes the code cleaner)

--- deck.py
+++ deck.py
@@ -1,14 +1,15 @@
 """
+In-class project:
 Deck of cards, use classes for deck, the suits, and the cards. Want to be able to shuffle the deck, and then pull a card from the top of the deck.
 """
 # import libraries
 import random, time
-# Define classes
 
-# make the Suit class (subclass of Deck)
+    # Define classes:
+# make the Suit class
 class Suit:
-    def __init__(self, typeSuit, color):
-        # give attributes of type, color, cards = 13
+    def __init__(self, typeSuit):
+        # Create a list of the card names
         self.__names__ = [
             'King',
             'Queen',
@@ -24,8 +25,15 @@ class Suit:
             'Two',
             'Ace'
         ]
+
+        # Assign the type of suit and color based on what's been given
         self.__suitType__ = typeSuit
-        self.__suitColor__ = color
+
+        # Determine what the actual color will be
+        if self.__suitType__ in ['Clubs','Spades']:
+            self.__suitColor__ = 'black'
+        else:
+            self.__suitColor__ = 'red'
 
     # give actions of generate cards
     def makeCards(self):
@@ -51,17 +59,12 @@ class Deck:
     def makeCardDeck(self):
         # make the suits
         for suit in self.__suitTypes__:
-            # print(suit)
-            # Assign based on color
-            if suit in ['Clubs','Spades']:
-                tempObject = Suit(suit,'black')
-                # self.__suitHolding__.append(Suit(suit,'black'))
-                # for suit in deck.__suitHolding__: print(suit.__suitType__, suit.__suitColor__)
-            else:
-                tempObject = Suit(suit,'red')
-                # self.__suitHolding__.append(Suit(suit, 'red'))
+            tempObject = Suit(suit)
             
             # Create the cards
+            # tempObject.makeCards() creates a generator OBJECT, which needs to be converted to a list for our use
+            # .extend() is a list method that adds the ITEMS of our list to __deck__
+            # NOTE: .append() will add the entire LIST as an ITEM
             self.__deck__.extend(list(tempObject.makeCards()))
         # print(self.__deck__)
     
@@ -69,14 +72,14 @@ class Deck:
         # go through and print each deck, waiting a short bit in between each printing
         for card in self.__deck__:
             print(card.showCard())
-            time.sleep(0.1)
+            time.sleep(0.2)
 
     def shuffleDeck(self):
         # shuffle the deck
         random.shuffle(self.__deck__)
     
     def pullCard(self):
-        # Pulls card from top of deck
+        # Pulls card from top of deck and removes it from the deck
         return self.__deck__.pop(0)
 
 
@@ -84,21 +87,28 @@ class Deck:
 # make the Card class (subclass of Suit)
 class Card(Suit):
     def __init__(self, name, typeSuit, color):
-        super().__init__(typeSuit, color)
-        # give attributes of suit, name(or rank)
+        # give attributes of suit, name, and color
         self.__cardName__ = name
+        self.__suitType__ = typeSuit
+        self.__suitColor__ = color
     
     def showCard(self):
+        # return a string of the full card name
         return f"{self.__cardName__} of {self.__suitType__}"
 # 
 def invalAction():
+    """
+    For when a user has entered an unaccaptable entry
+    """
     print("Please enter a valid entry.")
 
 def main():
+    # Build and shuffle the deck
     deck = Deck()
     deck.makeCardDeck()
     deck.shuffleDeck()
 
+    # Run the program until the user wants to quit
     while True:
         print("""
         What action would you like to do?
@@ -108,18 +118,26 @@ def main():
         4) Triple shuffle
         5) Quit
         """)
+        # 
         try:
             userAction = int(input())
 
             if userAction not in [1,2,3,4,5]:
                 invalAction()
+
         except ValueError:
             invalAction()
+
+        except:
+            # catch all for userAction entry
+            print("Another error has occurred during action entry.")
     
+        # Match the appropriate action
         match userAction:
             case 1:
                 card = deck.pullCard()
                 print(card.showCard())
+                # print(card.__suitColor__) # testing purposes
             case 2:
                 deck.printDeck()
             case 3:
@@ -130,5 +148,6 @@ def main():
             case 5:
                 break
 
+# Only run the main() function if this file is not imported
 if __name__ == "__main__":
     main()
